d8fa282e34004cb4893dbfb1dbad97ee84e1517a
JasperMooren <jasper.mooren@hotmail.nl>
2021-07-21 15:57:11 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def print_hello_name(name):
+    print(f'Hello, {name}!')
+
+
+def main():
+    print(repr(__name__))
+    print("Hello World!")
+    print("What is your name?")
+    name = input()
+    print_hello_name(name)
+
+
+if __name__ == '__main__':
+    main()
